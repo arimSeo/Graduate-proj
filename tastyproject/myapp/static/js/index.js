205affcd345a2105eda6ed290353276e09c5f4a9
arimmie <arimy981002@likelion.org>
2021-04-25 16:29:42 +0900

    first page

---
+++ tastyproject/myapp/static/js/index.js
@@ -0,0 +1,7 @@
+// $(document).ready(function () {
+//     $(".result_content").hide();
+//     setTimeout(function () {
+//         $(".loading").fadeOut(200);
+//         $(".result_content").fadeIn(4000);
+//     }, 2500)
+// }
